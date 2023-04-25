9ff9c410def33d6307f176693865de3a23c9b791
Ms Lawliet <alexieanoya@gmail.com>
2023-04-25 11:43:44 +0800

    wrote pseudocode

--- program_code.py
+++ program_code.py
@@ -1,2 +1,11 @@
 # Write a Python program that read a file containing the name of 20 students together with their GWA.
 # The program will outputs the name of the student who got the highest GWA (including the GWA).
+
+# open list_of_studs.txt (read)
+# read list_of_studs.txt by line
+# get name and gwa
+# store name and gwa in dict
+# store gwa in list for comparison
+# get highest gwa in gwa_list
+# search for name of stud with highest gwa in stud_dict
+# print out result
