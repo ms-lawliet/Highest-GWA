97fc970a31c2b3b3f874e9db396a719397bdb505
Ms Lawliet <alexieanoya@gmail.com>
2023-04-25 11:53:24 +0800

    created a method, opened list_of_studs.txt, and read it line by line

--- program_code.py
+++ program_code.py
@@ -1,11 +1,19 @@
 # Write a Python program that read a file containing the name of 20 students together with their GWA.
 # The program will outputs the name of the student who got the highest GWA (including the GWA).
 
-# open list_of_studs.txt (read)
-# read list_of_studs.txt by line
-# get name and gwa
-# store name and gwa in dict
-# store gwa in list for comparison
-# get highest gwa in gwa_list
-# search for name of stud with highest gwa in stud_dict
-# print out result
+
+def find_highest():
+    # open list_of_studs.txt
+    with open('list_of_studs.txt') as stud_list:
+        # read list_of_studs.txt by line
+        for line in stud_list:
+            print('a')
+            # get name and gwa
+            # store name and gwa in dict
+            # store gwa in list for comparison
+            # get highest gwa in gwa_list
+            # search for name of stud with highest gwa in stud_dict
+            # print out result
+
+
+find_highest()
